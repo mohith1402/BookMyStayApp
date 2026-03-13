890edf2a5b8e6f6e7ef2878c2148d070971b79ca
mohith1402 <ballolamohith1402@gmail.com>
2026-03-13 16:02:49 +0530

    Use Case 1: Application Entry & Welcome Message

---
+++ BMS/src/HotelBookingApp.java
@@ -0,0 +1,15 @@
+/*
+* Use Case 1: Application Entry & Welcome Message
+*
+* @auther Mohith
+* @version 1.0
+*
+ */
+
+public class HotelBookingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }
+}
